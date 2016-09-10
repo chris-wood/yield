2e2bcb06313d71a6bfbdad4dd9d645c66d6b5a3f
Christopher Wood <christopherwood07@gmail.com>
2016-09-10 12:08:33 -0700

    Updated packet generation script

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
 URI_FILE=$1
-INT_FILE=$2
-DATA_FILE=$3
+INT_FILE=${URI_FILE}.int
+DATA_FILE=${URI_FILE}.data
+
+INTEREST_BUILDER=$2
+CONTENT_BUILDER=$3
+CONTENT_SIZE=$4
 
 ### flag for COMPUTE_HASH
 ### param for DATA_SIZE
 
-# 1. for each line in URI_FILE
-# 2. create data packet with the name and data size
-# 3. compute hash of the packet
-# 4. create interest with the same name and hash
+while read l; do
+    if [ ${#l} -ne 0 ]; then
+        echo $l
+
+        # - create data packet with the name and data size
+        ${CONTENT_BUILDER} $l ${CONTENT_SIZE} > tmp.data
+
+        # - compute hash of the packet
+        dissect -i tmp.data -d SHA256 > tmp.hash
+
+        # - create interest with the same name and hash
+        ${INTEREST_BUILDER} $l "" `cat tmp.hash` > tmp.int
+
+        cat tmp.int >> ${INT_FILE}
+        cat tmp.data >> ${DATA_FILE}
+
+        rm -rf tmp.data
+        rm -rf tmp.hash
+    fi
+done < $URI_FILE
+
